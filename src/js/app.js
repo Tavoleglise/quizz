4328f644594de5166cfa26e657568842246bc76a
Tavoleglise <tavoleglise@gmail.com>
2020-11-05 13:02:16 -0600

    inicio de configuracion de respuestas

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,125 @@ const resultado1 = "resultado 1";
 const resultado2 = "resultado 2";
 const resultado3 = "resultado 3";
 
+const apivita_acondicionador_gentle = document.createElement("img");
+apivita_acondicionador_gentle.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-057 CONDITIONER GENTLE DAILY.png";
+const apivita_acondicionador_moisturizing = document.createElement("img");
+apivita_acondicionador_moisturizing.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-062 CONDITIONER MOISTURIZING ALL TYPES.png";
+const apivita_acondicionador_color = document.createElement("img");
+apivita_acondicionador_color.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-063 CONDITIONER COLOR PROTECT.png";
+const apivita_acondicionador_shine = document.createElement("img");
+apivita_acondicionador_shine.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-064 CONDITIONER SHINE _ REVITALIZING.png";
+const apivita_acondicionador_nourish = document.createElement("img");
+apivita_acondicionador_nourish.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-065 CONDITIONER NOURISH _ REPAIR.png";
+
+//Apivita aqua beelicious
+const apivita_aqua_beelicious_oilfree = document.createElement("img");
+apivita_aqua_beelicious_oilfree.src =
+  "src/images/APIVITA_AQUA_BEE_LICIOUS/BEELICIOUS_OILFREE_LIGHT_TUBE_AND_BOX.png";
+
+const apivita_aqua_beelicious_eyegel = document.createElement("img");
+apivita_aqua_beelicious_eyegel.src =
+  "src/images/APIVITA_AQUA_BEE_LICIOUS/AQUA_BEELICIOUS_EYE_GEL_TUBE_AND_BOX.png";
+
+const apivita_aqua_beelicious_booster = document.createElement("img");
+apivita_aqua_beelicious_booster.src =
+  "src/images/APIVITA_AQUA_BEE_LICIOUS/AQUA_BEELICIOUS_BOOSTER_TUBE_AND_BOX.png";
+
+//Apivita bee radiante
+const apivita_radiant_light = document.createElement("img");
+apivita_radiant_light.src =
+  "src/images/APIVITA_BEE_RADIANT/10-22-01-632 BEE RADIANT LIGHT JAR _ BOX.png";
+
+const apivita_radiant_eyetube = document.createElement("img");
+apivita_radiant_eyetube.src =
+  "src/images/APIVITA_BEE_RADIANT/10-22-01-634 BEE RADIANT EYE TUBE _ BOX.png";
+
+const apivita_radiant_serum = document.createElement("img");
+apivita_radiant_serum.src =
+  "src/images/APIVITA_BEE_RADIANT/10-22-01-636 BEE RADIANT SERUM BOTTLE _ BOX.png";
+
+//Apivita crema de manos
+const apivita_cremaManos_honey = document.createElement("img");
+apivita_cremaManos_honey.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-22-12-400 HAND CREAM HONEY.png";
+
+const apivita_cremaManos_jasmine = document.createElement("img");
+apivita_cremaManos_jasmine.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-22-12-401 HAND CREAM JASMINE.png";
+
+const apivita_cremaManos_hypericum = document.createElement("img");
+apivita_cremaManos_hypericum.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-22-12-402 HAND CREAM HYPERICUM.png";
+
+//Apivita lanzamiento 2020
+const apivita_lanzamiento2020_tonicmen = document.createElement("img");
+apivita_lanzamiento2020_tonicmen.src =
+  "src/images/APIVITA_LANZAMIENTOS_2020/10-10-08-151 SHAMPOO TONIC MEN.png";
+
+const apivita_lanzamiento2020_tonicwomen = document.createElement("img");
+apivita_lanzamiento2020_tonicwomen.src =
+  "src/images/APIVITA_LANZAMIENTOS_2020/10-10-08-152 SHAMPOO TONIC WOMEN.png";
+
+const apivita_lanzamiento2020_dry = document.createElement("img");
+apivita_lanzamiento2020_dry.src =
+  "src/images/APIVITA_LANZAMIENTOS_2020/10-10-08-154 SHAMPOO DRY DANDRUFF.png";
+
+const apivita_lanzamiento2020_oily = document.createElement("img");
+apivita_lanzamiento2020_oily.src =
+  "src/images/APIVITA_LANZAMIENTOS_2020/10-10-08-156 SHAMPOO OILY DANDRUFF.png";
+
+const apivita_lanzamiento2020_conditioner = document.createElement("img");
+apivita_lanzamiento2020_conditioner.src =
+  "src/images/APIVITA_LANZAMIENTOS_2020/10-10-09-059 CONDITIONER TONIC.png";
+
+const apivita_lanzamiento2020_lotion = document.createElement("img");
+apivita_lanzamiento2020_lotion.src =
+  "src/images/APIVITA_LANZAMIENTOS_2020/10-10-10-066 LOTION TONIC BOTTLE _ BOX.jpg";
+
+//Apivita leche corporal
+const apivita_leche_mountain = document.createElement("img");
+apivita_leche_mountain.src =
+  "src/images/APIVITA_LECHE_CORPORAL/10-22-12-392 TONIC MOUNTAIN TEA BODY MILK.png";
+
+const apivita_leche_fig = document.createElement("img");
+apivita_leche_fig.src =
+  "src/images/APIVITA_LECHE_CORPORAL/10-22-12-393 REFRESHING FIG BODY MILK.png";
+
+const apivita_leche_jasmine = document.createElement("img");
+apivita_leche_jasmine.src =
+  "src/images/APIVITA_LECHE_CORPORAL/10-22-12-394 PURE JASMINE BODY MILK.png";
+
+const apivita_leche_royal = document.createElement("img");
+apivita_leche_royal.src =
+  "src/images/APIVITA_LECHE_CORPORAL/10-22-12-397 ROYAL HONEY BODY CREAM.png";
+
+//Apivita limpiadores
+const apivita_limpiadores_face = document.createElement("img");
+apivita_limpiadores_face.src =
+  "src/images/APIVITA_LIMPIADORES/10-22-00-523 FACE _ EYE  FOAM CLEASING 200ML.png";
+
+const apivita_limpiadores_milk = document.createElement("img");
+apivita_limpiadores_milk.src =
+  "src/images/APIVITA_LIMPIADORES/10-22-00-526 CLEANSING MILK 3IN1 200ML.png";
+
+const apivita_limpiadores_gel = document.createElement("img");
+apivita_limpiadores_gel.src =
+  "src/images/APIVITA_LIMPIADORES/10-22-00-528 CLEANSING GEL.png";
+
+//Apivita mascarilla cabello
+const apivita_cabello_colored = document.createElement("img");
+apivita_cabello_colored.src =
+  "src/images/APIVITA_MASCARILLAS_CABELLO/10-10-09-114 MASK COLORED HAIR.png";
+
+const apivita_cabello_repair = document.createElement("img");
+apivita_cabello_repair.src =
+  "src/images/APIVITA_MASCARILLAS_CABELLO/10-10-09-115 MASK NOURISH _ REPAIR HAIR.png";
+
 const results = {
   respuestaP1: null,
   respuestaP2: null,
@@ -38,15 +157,27 @@ const activateAnswer = (numeroPregunta, numeroRespuesta) => {
 };
 
 const analizar = () => {
-  let diario = document.getElementById("diario");
+  let diario = document.getElementById("conjuntoProductosDiario");
   let semanal = document.getElementById("semanal");
   let resultado = `${results.respuestaP1}${results.respuestaP2}${results.respuestaP3}`;
+
   switch (resultado) {
     case "000":
       let productoDiario1 = document.createElement("div");
-      productoDiario1.textContent = "Sup, y'all?";
-      productoDiario1.setAttribute("class", "productoDiario");
-      document.getElementById("diario").appendChild(productoDiario1);
+      productoDiario1.setAttribute("class", "product");
+      diario.appendChild(productoDiario1);
+      productoDiario1.appendChild(apivita_acondicionador_gentle);
+
+      let productoDiario2 = document.createElement("div");
+      productoDiario2.setAttribute("class", "product");
+      diario.appendChild(productoDiario2);
+      productoDiario2.appendChild(apivita_cabello_colored);
+
+      let productoDiario3 = document.createElement("div");
+      productoDiario3.setAttribute("class", "product");
+      diario.appendChild(productoDiario3);
+      productoDiario3.appendChild(apivita_cabello_repair);
+
       break;
     case "100":
       document.getElementById("resultados").innerHTML = resultado2;
---
+++ src/js/images.js
@@ -0,0 +1,86 @@
+//Apivita acondicionadores
+const apivita_acondicionador_gentle = document.createElement("img");
+apivita_acondicionador_gentle.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-057 CONDITIONER GENTLE DAILY.png";
+const apivita_acondicionador_moisturizing = document.createElement("img");
+apivita_acondicionador_moisturizing.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-062 CONDITIONER MOISTURIZING ALL TYPES.png";
+const apivita_acondicionador_color = document.createElement("img");
+apivita_acondicionador_color.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-063 CONDITIONER COLOR PROTECT.png";
+const apivita_acondicionador_shine = document.createElement("img");
+apivita_acondicionador_shine.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-064 CONDITIONER SHINE _ REVITALIZING.png";
+const apivita_acondicionador_nourish = document.createElement("img");
+apivita_acondicionador_nourish.src =
+  "src/images/APIVITA_ACONDICIONADORES/10-10-09-065 CONDITIONER NOURISH _ REPAIR.png";
+
+//Apivita aqua beelicious
+const apivita_aqua_beelicious_oilfree = document.createElement("img");
+apivita_aqua_beelicious_oilfree.src =
+  "src/images/APIVITA_AQUA_BEE_LICIOUS/BEELICIOUS_OILFREE_LIGHT_TUBE_AND_BOX.png";
+
+const apivita_aqua_beelicious_eyegel = document.createElement("img");
+apivita_aqua_beelicious_eyegel.src =
+  "src/images/APIVITA_AQUA_BEE_LICIOUS/AQUA_BEELICIOUS_EYE_GEL_TUBE_AND_BOX.png";
+
+const apivita_aqua_beelicious_booster = document.createElement("img");
+apivita_aqua_beelicious_booster.src =
+  "src/images/APIVITA_AQUA_BEE_LICIOUS/AQUA_BEELICIOUS_BOOSTER_TUBE_AND_BOX.png";
+
+//Apivita bee radiante
+const apivita_radiant_light = document.createElement("img");
+apivita_radiant_light.src =
+  "src/images/APIVITA_BEE_RADIANT/10-22-01-632 BEE RADIANT LIGHT JAR _ BOX.png";
+
+const apivita_radiant_eyetube = document.createElement("img");
+apivita_radiant_eyetube.src =
+  "src/images/APIVITA_BEE_RADIANT/10-22-01-634 BEE RADIANT EYE TUBE _ BOX.png";
+
+const apivita_radiant_serum = document.createElement("img");
+apivita_radiant_serum.src =
+  "src/images/APIVITA_BEE_RADIANT/10-22-01-636 BEE RADIANT SERUM BOTTLE _ BOX.png";
+
+//Apivita crema de manos
+const apivita_cremaManos_honey = document.createElement("img");
+apivita_cremaManos_honey.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-22-12-400 HAND CREAM HONEY.png";
+
+const apivita_cremaManos_jasmine = document.createElement("img");
+apivita_cremaManos_jasmine.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-22-12-400 HAND CREAM JASMINE.png";
+
+const apivita_cremaManos_hypericum = document.createElement("img");
+apivita_cremaManos_hypericum.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-22-12-400 HAND CREAM HYPERICUM.png";
+
+//Apivita lanzamiento 2020
+const apivita_lanzamiento2020_tonicmen = document.createElement("img");
+apivita_lanzamiento2020_tonicmen.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-10-08-151 SHAMPOO TONIC MEN.png";
+
+const apivita_lanzamiento2020_tonicwomen = document.createElement("img");
+apivita_lanzamiento2020_tonicwomen.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-10-08-151 SHAMPOO TONIC WOMEN.png";
+
+const apivita_lanzamiento2020_dry = document.createElement("img");
+apivita_lanzamiento2020_dry.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-10-08-154 SHAMPOO DRY DANDRUFF.png";
+
+const apivita_lanzamiento2020_oily = document.createElement("img");
+apivita_lanzamiento2020_oily.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-10-08-154 SHAMPOO OILY DANDRUFF.png";
+
+const apivita_lanzamiento2020_conditioner = document.createElement("img");
+apivita_lanzamiento2020_conditioner.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-10-09-059 CONDITIONER TONIC.png";
+
+const apivita_lanzamiento2020_lotion = document.createElement("img");
+apivita_lanzamiento2020_lotion.src =
+  "src/images/APIVITA_CREMAS_MANOS/10-10-10-066 LOTION TONIC BOTTLE _ BOX.jpg";
+
+export {
+  apivita_acondicionador_gentle,
+  apivita_acondicionador_moisturizing,
+  apivita_acondicionador_color,
+};
